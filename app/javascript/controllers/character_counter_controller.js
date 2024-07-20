e361ae7efa7a173e71e14f6006c2113ee2a888fa
AbderrahmanKro <elhairechabderrahman@gmail.com>
2024-07-20 12:25:09 +0100

    update to add limit to counter

--- app/javascript/controllers/character_counter_controller.js
+++ app/javascript/controllers/character_counter_controller.js
@@ -2,6 +2,7 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   static targets = ["counter"];
+  static values = { limit: Number };
 
   connect() {
     console.log(this.element);
@@ -10,9 +11,15 @@ export default class extends Controller {
 
   updateCounter(event) {
     const numberOfCharacters = event.currentTarget.value.length;
-    if (numberOfCharacters > 0) {
-      const characterLabel = numberOfCharacters === 1 ? "character" : "characters";
-      this.counterTarget.innerHTML = `${numberOfCharacters} ${characterLabel}`;
+
+    if (numberOfCharacters > this.limitValue) {
+      const excessCharacters = numberOfCharacters - this.limitValue;
+      const characterLabel = excessCharacters === 1 ? "character" : "characters";
+      this.counterTarget.innerHTML = `Number of characters exceeded by ${excessCharacters} ${characterLabel}`;
+    } else if (numberOfCharacters > 0) {
+      const remainingCharacters = this.limitValue - numberOfCharacters;
+      const characterLabel = remainingCharacters === 1 ? "character" : "characters";
+      this.counterTarget.innerHTML = `${remainingCharacters} ${characterLabel} left`;
     } else {
       this.counterTarget.innerHTML = "";
     }
